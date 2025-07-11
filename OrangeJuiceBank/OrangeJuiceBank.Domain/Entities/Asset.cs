2a4a963d1384ee8521eb04ff3d40618fca67bbca
João Filho <joaofilholimati@gmail.com>
2025-07-10 22:44:46 -0300

    feat: criar entidades de domínio com relacionamentos e enums

---
+++ OrangeJuiceBank/OrangeJuiceBank.Domain/Entities/Asset.cs
@@ -0,0 +1,14 @@
+﻿public enum AssetType
+{
+    Acao,
+    Cdb,
+    TesouroDireto
+}
+
+public class Asset
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public AssetType Type { get; set; }
+    public decimal CurrentPrice { get; set; }
+}
---
+++ OrangeJuiceBank/OrangeJuiceBank.Domain/Entities/Investment.cs
@@ -0,0 +1,13 @@
+﻿public class Investment
+{
+    public Guid Id { get; set; }
+    public Guid AccountId { get; set; }
+    public Account Account { get; set; }
+
+    public Guid AssetId { get; set; }
+    public Asset Asset { get; set; }
+
+    public decimal Quantity { get; set; }
+    public decimal AveragePrice { get; set; } 
+    public DateTime CreatedAt { get; set; }
+}
