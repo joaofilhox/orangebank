664894cc7cbc4512a62969d6241bd91e7199cfe7
João Filho <joaofilholimati@gmail.com>
2025-07-11 21:30:53 -0300

    feat: adicionar cpf e data de nascimento ao User

--- OrangeJuiceBank/OrangeJuiceBank.Domain/Entities/User.cs
+++ OrangeJuiceBank/OrangeJuiceBank.Domain/Entities/User.cs
@@ -3,6 +3,10 @@
     public Guid Id { get; set; }
     public string FullName { get; set; }
     public string Email { get; set; }
+    public string Cpf { get; set; }
+    public DateTime DateOfBirth { get; set; }
 
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
     public ICollection<Account> Accounts { get; set; }
 }
