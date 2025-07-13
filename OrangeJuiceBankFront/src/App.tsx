3f29f2351f6200143bf3f793ad81e4224f364c7b
João Filho <joaofilholimati@gmail.com>
2025-07-13 12:11:27 -0300

    feat: implementar guardião de rotas para proteger páginas autenticadas

--- OrangeJuiceBankFront/src/App.tsx
+++ OrangeJuiceBankFront/src/App.tsx
@@ -1,21 +1,28 @@
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
-import { ProtectedRoute, PublicOnlyRoute } from './components/ProtectedRoute'
+import ProtectedRoute from './routes/ProtectedRoute'
+import PublicOnlyRoute from './routes/PublicOnlyRoute'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={
-        <PublicOnlyRoute>
-          <LoginPage />
-        </PublicOnlyRoute>
-      } />
-      <Route path="/dashboard" element={
-        <ProtectedRoute>
-          <DashboardPage />
-        </ProtectedRoute>
-      } />
+      <Route
+        path="/"
+        element={
+          <PublicOnlyRoute>
+            <LoginPage />
+          </PublicOnlyRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   )
 }
---
+++ OrangeJuiceBankFront/src/routes/PublicOnlyRoute.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import { getToken } from '../utils/token'
+
+interface PublicOnlyRouteProps {
+    children: React.ReactElement
+}
+
+export default function PublicOnlyRoute({ children }: PublicOnlyRouteProps) {
+    const token = getToken()
+
+    if (token) {
+        return <Navigate to="/dashboard" replace />
+    }
+
+    return children
+} 
